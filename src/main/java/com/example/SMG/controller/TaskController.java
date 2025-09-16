364f84dc3e9c1becbd2034bc3a8bb89f913afcbb
shiraiwa_kazuki <shiraiwa.kazuki@alhinc.jp>
2025-09-16 17:18:49 +0900

    Entityを削除 controllerに追記

--- src/main/java/com/example/SMG/controller/TaskController.java
+++ src/main/java/com/example/SMG/controller/TaskController.java
@@ -1,4 +1,23 @@
 package com.example.SMG.controller;
 
+import com.example.SMG.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class TaskController {
+
+    @Autowired
+    TaskService taskService;
+
+    @GetMapping
+    public ModelAndView top(){
+        ModelAndView mav = new ModelAndView();
+        List<TaskForm> contentData = taskService.findTask();
+        mav.setViewName("/");
+        mav.addObject("tasks", contentData);
+        return mav;
+    }
 }
