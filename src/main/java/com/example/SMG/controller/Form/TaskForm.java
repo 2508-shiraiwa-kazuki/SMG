f75083c0e6204401b6785e6c9691293844e7f96e
sato_issei <sato.issei@alhinc.jp>
2025-09-19 11:59:16 +0900

    修正

--- src/main/java/com/example/SMG/controller/Form/TaskForm.java
+++ src/main/java/com/example/SMG/controller/Form/TaskForm.java
@@ -14,12 +14,8 @@ public class TaskForm {
     private Integer id;
 
     @NotBlank(message = "タスクを入力してください")
-<<<<<<< HEAD
-    @Size(min = 1,  max = 140, message = "タスクは140文字以内で入力してください")
-=======
     @Pattern(regexp = "^(?![　]+$).*", message = "タスクを入力してください")
     @Size(min = 0,  max = 140, message = "タスクは140文字以内で入力してください")
->>>>>>> 8354c96e0ded6f40708db9c3e4ded4e3c32165c3
     private String content;
 
     private int status;
